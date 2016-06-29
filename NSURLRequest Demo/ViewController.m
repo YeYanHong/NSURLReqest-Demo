41db948bcd28b037864fe82a9ea8bc40ba867d55
hong <yeyanhong@yeyanhongs-macbook-pro.local>
2016-06-29 21:34:22 +0800

    Initial Commit

---
+++ NSURLRequest Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NSURLRequest Demo
+//
+//  Created by SKY on 16/6/29.
+//  Copyright © 2016年 RDP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ NSURLRequest Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSURLRequest Demo
+//
+//  Created by SKY on 16/6/29.
+//  Copyright © 2016年 RDP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
